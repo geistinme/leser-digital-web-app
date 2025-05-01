8e7fb2423ccfb337e58393eaaf878c3ac4f4eae8
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-05-01 22:53:30 +0200

    fix: disable mobile for now

--- src/mobile/App.tsx
+++ src/mobile/App.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 
 import { ApolloProvider } from "@apollo/client";
-import { RouterProvider } from "react-router";
+import { Flex, Typography } from "@sampled-ui/base";
 
 import { client } from "../apollo";
-
-import { router } from "./routes";
+import SvgWordmarkLogo from "../icons/WordmarkLogo";
 
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <RouterProvider router={router} />
+      <Flex
+        direction="column"
+        align="center"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "#333",
+          color: "#fff",
+          textAlign: "center",
+        }}
+      >
+        <SvgWordmarkLogo width="70%" height="80%" viewBox="0 0 586 160" />
+        <Typography.Text>
+          Bald auch auf mobilen Geräten verfügbar. Probiere solange die Desktop
+          Version aus.
+        </Typography.Text>
+      </Flex>
+      {/* <RouterProvider router={router} /> */}
     </ApolloProvider>
   );
 };
